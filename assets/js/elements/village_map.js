03c2965d7c70d2a7641fb294cb6d2f8cf2d20a9e
LechDutkiewicz <lechdutkiewicz@gmail.com>
2015-09-21 16:43:25 +0200

    update in village-map template

--- assets/js/elements/village_map.js
+++ assets/js/elements/village_map.js
@@ -3,19 +3,15 @@
 	$(document).ready(function(){
 		if (typeof cottages !== 'undefined')
 		{
+
+			// scroll viewport to top of village image
 			$('html, body').animate({
-				scrollTop: $("#village-map").offset().top
+				scrollTop: $("#map-header").offset().top - 50
 			}, 2000);
 
-			var plots = $('.plot');
+			// setup vars for each cottage from cottage object given in block-village-map.php
 
 			Object.keys(cottages).forEach(function(key) {
-
-				// var plot = $('#plot-' + key),
-				// cottage = $('#cottage-' + key),
-				// link = cottage.find('.cottage-link'),
-				// fence = plot.find('.fence'),
-				// camera = cottage.find('.camera-holder');
 				
 				var plot = $('[id="plot-' + key + '"]'),
 				cottage = $('[id="cottage-' + key + '"]'),
@@ -24,8 +20,11 @@
 				fence = plot.find('[class^="fence"]'),
 				camera = cottage.find('[class="camera-holder"]');
 
+				// specify if target contains images or video and apply according class
 				camera.attr('class', 'camera-holder ' + cottages[key]['format']);
+				camera.addClass(cottages[key]['format']);
 
+				// show svg group with house for each link specified in block-village-map.php
 				setTimeout(function () {
 					fence.fadeIn(500, 'easeInQuart');						
 				}, key * 200 + 600 );
@@ -34,11 +33,65 @@
 					cottage.fadeIn(500, 'easeInQuart');						
 				}, key * 200 + 600 );
 
-				title.html(cottages[key]['title']);
+				// set link attributes for svg element needed for it's ajax call to fetch target's data
 				link.attr('xlink:href', cottages[key]['url']);
-				camera.addClass(cottages[key]['format']);
+				link.attr('data-post-title', cottages[key]['title']);
+				link.attr('data-post-id', cottages[key]['id']);
+				// set title tag according to link target
+				title.html(cottages[key]['translatedView'] + ' ' + cottages[key]['title']);
 			});
-		}
+}
+});
+
+// bind to hover on each link an action to show preview with target's thumbnail
+$('.plot .cottage-link .camera-holder').hover(function() {
+
+	var el = $(this).parent(),
+	targetPostTitle = el.data('post-title'),
+	canvas = $('#canvas'),
+	// html element of thumbnail image holder
+	imgHolder = "<div class='cot-details-holder loading'></div>",
+	send_data = new Object;
+
+	// setup data for post call
+	send_data.action = 'get-cot-details';
+	send_data.id = el.data('post-id');
+	send_data.thumbSize = thumbSize;
+
+	// determine scale of svg display relative to it's original viewbox size parameter
+	var ratio = canvas.width() / 790.9,
+	// check canvas height
+	canvasHeight = canvas.height(),
+	// getBBox method to fetch size of selected svg element
+	BBox = $(this).parent().parent()[0].getBBox(),
+	// count actial svg .plot element height
+	plotHeight = $(this).parent().parent()[0].getBBox().height * ratio,
+	position = new Object;
+
+	// setup absolute position of imgHolder element
+	position.bottom = canvasHeight - ( BBox.y * ratio );
+	position.left = BBox.x * ratio;
+
+	canvas.append(imgHolder);
+
+	var box = canvas.find('.cot-details-holder');
+	box.css({
+		bottom: position.bottom + "px",
+		left: position.left + 'px'
 	});
+
+	$.post(bon_toolkit_ajax.url, send_data, function (data) {
+		box.removeClass('loading').html(data.thumbnail).attr('title', targetPostTitle);
+		setTimeout(function () {
+			box.fadeIn(300);						
+		}, 100);
+	}, 'json');
+}, function(){
+	var canvas = $('#canvas');
+	var box = canvas.find('.cot-details-holder');
+	box.fadeOut(100, function(){
+		$(this).remove()
+	});
+});
 
 })(jQuery);
