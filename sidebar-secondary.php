7e534429cb8b97a00a71d0a374642b01912b3ea6
LechDutkiewicz <lechdutkiewicz@gmail.com>
2016-02-22 15:20:23 +0100

    made sidebar dompatible with jquery.sticky

--- sidebar-secondary.php
+++ sidebar-secondary.php
@@ -1,9 +1,13 @@
 <?php if ( is_active_sidebar( 'secondary' ) ) : ?>
 
 	<aside id="sidebar-secondary" class="sidebar <?php echo shandora_column_class('large-4'); ?>">
-
-		<?php dynamic_sidebar( 'secondary' ); ?>
-
+		<?php if ( is_single() ) { ?>
+		<div class="aside-wrapper sticky-wrapper">
+			<?php } ?>
+			<?php dynamic_sidebar( 'secondary' ); ?>
+			<?php if ( is_single() ) { ?>
+		</div>
+		<?php } ?>
 	</aside><!-- #sidebar-primary .aside -->
 
 <?php endif; ?>
