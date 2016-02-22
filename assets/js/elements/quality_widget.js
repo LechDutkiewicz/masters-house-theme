1990228ab7b1ba0d2c5cb8c9a464aa871ef2e1f0
LechDutkiewicz <lechdutkiewicz@gmail.com>
2016-02-22 15:17:24 +0100

    changed house with bubbles javascript for about us page

--- assets/js/elements/quality_widget.js
+++ assets/js/elements/quality_widget.js
@@ -1,71 +1,72 @@
 (function($) {
 
-	var placeDots = function(dots) {
+	var bubbleHouse = {
 
-		width = container.width();
+		args: {
+			container: $('#quality-container'),
+			dots: $('#quality-container .quality-icon'),
+			descriptionsContainer: $('#quality-container .desc-container')
+		},
+		init: function() {
+			bubbleHouse.placeDots();
+			bubbleHouse.bindClickEvents();
+			bubbleHouse.onResize();
+		},
+		placeDots: function() {
 
-		if ( width >= 940 ) {
+			var width = bubbleHouse.args.container.width();
 
-			dots.each(function() {
-				$(this).css({
-					'top'	:$(this).data('top') + "%",
-					'left'	:$(this).data('left') + "%",
+			if ( width >= 940 ) {
+				bubbleHouse.args.dots.each(function() {
+					$(this).css({
+						'top'	:$(this).data('top') + "%",
+						'left'	:$(this).data('left') + "%",
+						'opacity'	: 1
+					});
+				});
+			} else if ( width >= 520 ) {
+				bubbleHouse.args.dots.each(function() {
+					$(this).css({
+						'top'	:$(this).data('top') + "%",
+						'left'	:$(this).data('left') + "%",
+						'opacity'	: 1
+					});
+				});
+			}
+		},
+		bindClickEvents: function() {
+
+			bubbleHouse.args.dots.click(function(){
+
+				var target = $(this).data('target');
+				
+				bubbleHouse.args.container.find('.click-arrow').hide();
+				bubbleHouse.args.descriptionsContainer.find('.active .description-img').fadeOut(500, function(){
+
+					bubbleHouse.args.descriptionsContainer.find('.active').fadeOut(500, function(){
+
+						var chosenItem = bubbleHouse.args.descriptionsContainer.find("[data-target='" + target + "']");
+
+						$(this).removeClass('active');
+						if ( !chosenItem.hasClass('active') ) {
+							chosenItem.fadeIn(500, function(){
+								chosenItem.find('.description-img').fadeIn();	
+							}).addClass('active');
+						}
+					});
 				});
 			});
-
-		} else if ( width >= 520 ) {
-
-			dots.each(function() {
-				$(this).css({
-					'top'	:$(this).data('top') + "%",
-					'left'	:$(this).data('left') + "%",
-				});
+		},
+		onResize: function() {
+			$(window).resize(function(){
+				bubbleHouse.placeDots();
 			});
-
-		}
+		},
 
 	};
 
-	/* Modal with quality description */
-	var container = $('#quality-modal'),
-	dots = $('.quality-icon', container),
-	descriptionsContainer = $('.desc-container', container);
-
-	$('#quality-modal').on('opened.fndtn.reveal', function() {
-		placeDots(dots);
-	});
-
-	$(window).resize(function(){
-		placeDots(dots);
-	});
-
-	// dots.hover(function(){
-	// 	$(this).toggleClass('active').find('.icon-desc-container').fadeIn();
-	// }, function(){
-	// 	$(this).toggleClass('active').find('.icon-desc-container').hide();
-	// });
-
-	dots.click(function(){
-		
-		container.find('.click-arrow').hide();
-
-		var target = $(this).data('target');
-		descriptionsContainer.find('.active .description-img').fadeOut(500, function(){
-
-			descriptionsContainer.find('.active').fadeOut(500, function(){
-
-				$(this).removeClass('active');
-
-				var chosenItem = descriptionsContainer.find("[data-target='" + target + "']");
-				if ( !chosenItem.hasClass('active') ) {
-					chosenItem.fadeIn(500, function(){
-						chosenItem.find('.description-img').fadeIn();	
-					}).addClass('active');
-				}
-
-			});
-
-		});
+	$(document).ready(function(){
+		bubbleHouse.init();
 	});
 
 	/* End of modal with quality description */
