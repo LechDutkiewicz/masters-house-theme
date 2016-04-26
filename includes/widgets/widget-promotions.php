38ea62caea17cd371b16033f4697f6d04b7fd062
LechDutkiewicz <lechdutkiewicz@gmail.com>
2016-04-26 21:07:57 +0200

    turn off promotions widget in sidebar when promotions are disabled in admin theme options

--- includes/widgets/widget-promotions.php
+++ includes/widgets/widget-promotions.php
@@ -42,7 +42,7 @@ class TSM_Promotions_Posts extends WP_Widget {
 			);
 
 		/* Create the widget. */
-		$this->WP_Widget(
+		parent::__construct(
 				'tsm-promotions', // $this->id_base
 				'TSM Promotions', // $this->name
 				$widget_options, // $this->widget_options
@@ -57,71 +57,77 @@ class TSM_Promotions_Posts extends WP_Widget {
 	 */
 	function widget( $sidebar, $instance ) {
 
-		extract( $sidebar );
+		if ( bon_get_option( 'home_promotion' ) ) {
 
-		/* Set the $args for wp_get_archives() to the $instance array. */
-		$args = $instance;
-		$button_color = bon_get_option( 'search_button_color', 'red' );
+			extract( $sidebar );
 
-		/* Overwrite the $echo argument and set it to false. */
-		$args['echo'] = false;
+			/* Set the $args for wp_get_archives() to the $instance array. */
+			$args = $instance;
+			$button_color = bon_get_option( 'search_button_color', 'red' );
 
-		/* Output the theme's $before_widget wrapper. */
-		echo $before_widget;
+			/* Overwrite the $echo argument and set it to false. */
+			$args['echo'] = false;
 
-		/* If a title was input by the user, display it. */
-		if ( !empty( $instance['title'] ) )
-			echo $before_title . apply_filters( 'widget_title', $instance['title'], $instance, $this->id_base ) . $after_title;
+			/* Output the theme's $before_widget wrapper. */
+			echo $before_widget;
 
-		$args = array(
-			'post_status' => 'publish',
-			'post_type' => 'promotions',
-			'posts_per_page' => 1,
-			'order' => 'ASC',
-			'orderby' => 'date',
-			'ignore_sticky_posts' => true,
-			);
+			/* If a title was input by the user, display it. */
+			if ( !empty( $instance['title'] ) )
+				echo $before_title . apply_filters( 'widget_title', $instance['title'], $instance, $this->id_base ) . $after_title;
 
-		$promotions_query = get_posts( $args );
+			$args = array(
+				'post_status' => 'publish',
+				'post_type' => 'promotions',
+				'posts_per_page' => 1,
+				'order' => 'ASC',
+				'orderby' => 'date',
+				'ignore_sticky_posts' => true,
+				);
 
-		foreach ( $promotions_query as $post ) {
-			$thumb_id = get_post_thumbnail_id( $post->ID );
-			$thumb_full = wp_get_attachment_image_src( $thumb_id, 'full' );
-			?>
+			$promotions_query = get_posts( $args );
 
-			<div class="item clear">
-				<header class="entry-header">
-					<?php
-					if ( current_theme_supports( 'get-the-image' ) && get_post_field( 'post_content', $post->ID ) ) {
-						if ( $_SESSION['layoutType'] === 'mobile' ) {
-							get_the_image( array( 'post_id' => $post->ID, 'size' => 'mobile_regular', 'image_scan' => true ) );
-						} else {
-							get_the_image( array( 'post_id' => $post->ID, 'size' => 'full', 'image_scan' => true ) );
-						}
-					} else if ( current_theme_supports( 'get-the-image' ) && !get_post_field( 'post_content', $post->ID ) ) { ?>
-					<div class="zoom-img">
-						<a href="<?php echo $thumb_full[0]; ?>">
-							<?php
+			foreach ( $promotions_query as $post ) {
+				$thumb_id = get_post_thumbnail_id( $post->ID );
+				$thumb_full = wp_get_attachment_image_src( $thumb_id, 'full' );
+				?>
+
+				<div class="item clear">
+					<header class="entry-header">
+						<?php
+						if ( current_theme_supports( 'get-the-image' ) && get_post_field( 'post_content', $post->ID ) ) {
 							if ( $_SESSION['layoutType'] === 'mobile' ) {
-								get_the_image( array( 'post_id' => $post->ID, 'size' => 'mobile_regular', 'image_scan' => true, 'link_to_post' => false ) );
+								get_the_image( array( 'post_id' => $post->ID, 'size' => 'mobile_regular', 'image_scan' => true ) );
 							} else {
-								get_the_image( array( 'post_id' => $post->ID, 'size' => 'full', 'image_scan' => true, 'link_to_post' => false ) );
-							} ?>
-						</a>
+								get_the_image( array( 'post_id' => $post->ID, 'size' => 'full', 'image_scan' => true ) );
+							}
+						} else if ( current_theme_supports( 'get-the-image' ) && !get_post_field( 'post_content', $post->ID ) ) { ?>
+						<div class="zoom-img">
+							<a href="<?php echo $thumb_full[0]; ?>">
+								<?php
+								if ( $_SESSION['layoutType'] === 'mobile' ) {
+									get_the_image( array( 'post_id' => $post->ID, 'size' => 'mobile_regular', 'image_scan' => true, 'link_to_post' => false ) );
+								} else {
+									get_the_image( array( 'post_id' => $post->ID, 'size' => 'full', 'image_scan' => true, 'link_to_post' => false ) );
+								} ?>
+							</a>
+						</div>
+						<?php } ?>
+					</header>
+					<?php if ( get_post_field( 'post_content', $post->ID ) ) { ?>
+					<div class="item-content padding-medium top">
+						<a href="<?php echo get_the_permalink( $post->ID ); ?>" class="button flat <?php echo $button_color; ?> radius" title="<?php echo get_the_title( $post->ID ); ?>"><?php _e( 'Read more', 'bon' ); ?></a>
 					</div>
 					<?php } ?>
-				</header>
-				<?php if ( get_post_field( 'post_content', $post->ID ) ) { ?>
-				<div class="item-content padding-medium top">
-					<a href="<?php echo get_the_permalink( $post->ID ); ?>" class="button flat <?php echo $button_color; ?> radius" title="<?php echo get_the_title( $post->ID ); ?>"><?php _e( 'Read more', 'bon' ); ?></a>
 				</div>
-				<?php } ?>
-			</div>
 
-			<?php }
+				<?php }
 
-			/* Close the theme's widget wrapper. */
-			echo $after_widget;
+				/* Close the theme's widget wrapper. */
+				echo $after_widget;
+
+			} else {
+				return;
+			}
 		}
 
 	/**
